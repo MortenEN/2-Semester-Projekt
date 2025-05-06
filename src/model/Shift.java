e84504ad699a5861aab5b3ec02a4ed036bb60a56
benjaminwalther9490 <10632096@ucn.dk>
2025-05-06 09:08:20 +0200

    1.4
    Shift model lag

--- src/model/Shift.java
+++ src/model/Shift.java
@@ -1,5 +1,29 @@
 package model;
 
+import java.time.LocalDateTime;
+
 public class Shift {
+	private LocalDateTime start;
+	private LocalDateTime end;
+	
+	public Shift(LocalDateTime start) {
+		this.start = start;
+	}
+
+	public LocalDateTime getStart() {
+		return start;
+	}
+
+	public void setStart(LocalDateTime start) {
+		this.start = start;
+	}
+
+	public LocalDateTime getEnd() {
+		return end;
+	}
+
+	public void setEnd(LocalDateTime end) {
+		this.end = end;
+	}
 
 }
