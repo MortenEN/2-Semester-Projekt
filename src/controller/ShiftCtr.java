2df727c8b56b5e67ab056ca9bee4ec91334887cc
benjaminwalther9490 <10632096@ucn.dk>
2025-05-06 11:57:51 +0200

    1.9
    Controller lag med metoder

--- src/controller/ShiftCtr.java
+++ src/controller/ShiftCtr.java
@@ -1,5 +1,23 @@
 package controller;
 
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+
+import dataBase.ShiftDB;
+import dataBase.ShiftDBIF;
+import model.Shift;
+
 public class ShiftCtr {
+	private ShiftDBIF shiftDB;
+	
+	public ShiftCtr() throws SQLException {
+		shiftDB = new ShiftDB();
+	}
+	
+	public Shift createShift(LocalDateTime now) {
+		Shift shift;
+		shift = shiftDB.createShiftObject(now);
+		return shift;
+	}
 
 }
--- src/controller/WorkerCtr.java
+++ src/controller/WorkerCtr.java
@@ -1,5 +1,22 @@
 package controller;
 
-public class WorkerCtr {
+import java.sql.SQLException;
 
+import dataBase.WorkerDB;
+import dataBase.WorkerDBIF;
+import model.Worker;
+
+public class WorkerCtr {
+private WorkerDBIF workerDB;
+	
+	public WorkerCtr() throws SQLException {
+		workerDB = new WorkerDB();
+	}
+	
+	public Worker findWorker(String workerNumber) throws SQLException {
+		Worker worker;
+		
+		worker = workerDB.findWorkerByWorkerNumber(workerNumber);
+		return worker;
+	}
 }
