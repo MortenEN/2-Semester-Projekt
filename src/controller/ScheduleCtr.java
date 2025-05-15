4860e73f1fc4de85dc2f123606ab1467dcb8b97c
tmcn99 <tmcn134@gmail.com>
2025-05-15 08:55:09 +0200

    4.9
    scheduleDBIF & ctr

--- src/controller/ScheduleCtr.java
+++ src/controller/ScheduleCtr.java
@@ -1,5 +1,15 @@
 package controller;
 
+import java.sql.SQLException;
+
+import dataBase.ScheduleDBIF;
+
 public class ScheduleCtr {
+	private ScheduleDBIF scheduleDB;
+	
+	public ScheduleCtr() throws SQLException {
+		
+		
+	}
 
 }
