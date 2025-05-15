ebd8adbbc97c707d8e510354a4f66847afc52535
Andreas-SB <10632087@ucn.dk>
2025-05-15 09:04:18 +0200

    5.0
    Nu med dbif

--- src/dataBase/ScheduleDB.java
+++ src/dataBase/ScheduleDB.java
@@ -1,5 +1,43 @@
 package dataBase;
 
-public class ScheduleDB {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import model.Schedule;
+import model.Shift;
+
+public class ScheduleDB implements ScheduleDBIF{
+	private Connection con;
+	private DateTimeFormatter formatter;
+	
+	public ScheduleDB() throws SQLException {
+		con = DBConnection.getInstance().getConnection();
+		create = con.prepareStatement(create_SQL);
+		update = con.prepareStatement(update_SQL);
+		formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+	}
+
+	@Override
+	public void addScheduleToDB(LocalDate start, LocalDate end, String name) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Schedule> createScheduleObject(String workerNumber) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateScheduleInDB(String start, LocalDateTime end, String name) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/dataBase/ScheduleDBIF.java
+++ src/dataBase/ScheduleDBIF.java
@@ -1,5 +1,13 @@
 package dataBase;
 
-public class ScheduleDBIF {
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import model.Schedule;
 
+public interface ScheduleDBIF {
+	void addScheduleToDB(LocalDate start,LocalDate end, String name) throws SQLException;
+	List<Schedule>createScheduleObject(String workerNumber) throws SQLException;
+	void updateScheduleInDB(String start, LocalDateTime end, String name) throws SQLException;
 }
