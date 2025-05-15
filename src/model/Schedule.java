8b090945ea7987c7bb738ebaeeff08908fe473e9
Andreas-SB <10632087@ucn.dk>
2025-05-15 08:47:06 +0200

    4.9
    Schedule med attributter

--- src/model/Schedule.java
+++ src/model/Schedule.java
@@ -1,5 +1,30 @@
 package model;
 
-public class Schedule {
+import java.time.LocalDateTime;
 
+public class Schedule {
+	private LocalDateTime start;
+	private LocalDateTime end;
+	private String name;
+	
+	
+	public LocalDateTime getStart() {
+		return start;
+	}
+	public void setStart(LocalDateTime start) {
+		this.start = start;
+	}
+	public LocalDateTime getEnd() {
+		return end;
+	}
+	public void setEnd(LocalDateTime end) {
+		this.end = end;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
